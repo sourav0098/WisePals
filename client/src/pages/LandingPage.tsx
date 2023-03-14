c5b8663e66a16c6e7f607f146f19df00d6956101
Moanisha <moanisha94@gmail.com>
2023-03-14 08:31:10 -0400

    add api endpoints in landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,13 +4,16 @@ import landingPicture from "../assets/landingPicture.png";
 import SearchBar from "../features/searchTutors/components/SearchBar";
 import axios from "axios";
 import { useDispatch } from "react-redux";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
 
 const LandingPage: React.FC = () => {
   const [content, setContent] = useState("");
   const dispatch = useDispatch();
   useEffect(() => {
     axios
-      .get("http://localhost:5001/api/v1/contents/63e7a5d14236e852403de3c6")
+      .get(
+        `${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.CONTENTS}/63e7a5d14236e852403de3c6`
+      )
       .then((response) => {
         setContent(response.data.data.description);
       });
--- client/src/utils/apiEndpoints.ts
+++ client/src/utils/apiEndpoints.ts
@@ -1,6 +1,7 @@
 export const API_ENDPOINTS = {
   BASE_URL: "http://localhost:5001",
   TUTORS: "/api/v1/tutors",
+  CONTENTS:"/api/v1/contents",
   IMAGES: "/img/",
   TEST_AUTH: "/api/v1/tutors/testAuth",
   REGISTER: "/api/v1/auth/register",
